c00f505f089dd0d3f1c1254228412bfa908ca1d1
kbu <whalsrl06@kbu.ac.kr>
2025-10-15 11:39:53 +0900

    initial commit

---
+++ week02/sample2/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    // 메소드
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        String name = "";
+
+        name = keyboard.nextLine();
+        // Data 입력
+
+        // 처리 ;
+
+        // 출력 (필수) <- 필수니까 제일 1순위로 적기
+        System.out.println("입력받은 이름 : " + name);
+    }
+}
